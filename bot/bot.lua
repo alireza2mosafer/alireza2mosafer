7190773381fa732b4f7108b83eae5570c5f36fd2
alireza2mosafer <alireza2mosafer@gmail.com>
2016-09-18 18:22:07 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,8 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
+    sudo_users = {Bot ID: 161281996
+Your ID: @tak_pars},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
